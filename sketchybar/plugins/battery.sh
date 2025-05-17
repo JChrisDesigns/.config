ae325d2ac8e883302968d06740c802e3bcaac3e0
JChrisDesigns <145168383+JChrisDesigns@users.noreply.github.com>
2025-05-16 22:34:22 -0500

    updated a few things

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -8,13 +8,13 @@ if [ "$PERCENTAGE" = "" ]; then
 fi
 
 case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
+  9[0-9]|100) ICON="" COLOR="0xFF23FF07"
   ;;
-  [6-8][0-9]) ICON=""
+  [6-8][0-9]) ICON="" COLOR="0xFFC2FF07"
   ;;
-  [3-5][0-9]) ICON=""
+  [3-5][0-9]) ICON="" COLOR="0xFFFFFF09"
   ;;
-  [1-2][0-9]) ICON=""
+  [1-2][0-9]) ICON="" COLOR="0xFFFB1907"
   ;;
   *) ICON=""
 esac
@@ -25,4 +25,4 @@ fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%" icon.color="$COLOR"
