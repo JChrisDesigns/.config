0a40e2908db919d6d9b1ae89e89880d9fd118985
JChrisDesigns <145168383+JChrisDesigns@users.noreply.github.com>
2025-05-16 23:38:35 -0500

    Newest changes.

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -5,16 +5,19 @@
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
     sketchybar --animate exp 5 \
-               --set $NAME \
-               background.color=0xFFD9342E \
-               icon.padding_right=12 \
+               --set bracket.$1 \
+               background.color=0xFF1E2127 \
+               --set space.$1 \
                icon.padding_left=12 \
-               label.background.border_width=0
+               icon.padding_right=12
+
 else
     sketchybar --animate exp 5 \
-               --set $NAME \
+               --set space.$1 \
                label.background.border_width=0 \
                background.color=0x00D9342E \
                icon.padding_left=8 \
-               icon.padding_right=8
+               icon.padding_right=8 \
+               --set bracket.$1 \
+               background.color=0x751E2127
 fi
